9d085f2d2cbee5f86189464daf3c73b2c4a3761f
0xcoded <130364027+0xcoded@users.noreply.github.com>
2024-09-14 14:14:39 +0200

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,23 @@
+import socket
+
+IP = input("IP: ")
+numPorts = 0
+porcentaje = 0
+for Port in range(0,65535):
+        #13 - 100
+        #1 - x
+
+        if Port % 5000 == 0:
+                porcentaje = round((((1*100)/13) * numPorts))
+                print(str(porcentaje) + "% escaneado")
+                numPorts += 1
+        try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                        s.settimeout(0.5)
+                        #s.connect(("na18.holy.gg", 26247))
+                        #s.connect((str(IP), 26247))
+                        s.connect((str(IP), int(Port)))
+
+                        print("Servidor está ABIERTO (" + IP + ":" + str(Port) + ")")
+        except:
+                pass
